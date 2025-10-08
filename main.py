0f865639a33620adba78510a3eb87c648d683496
sillyderg <rhylie.beevers@gmail.com>
2025-10-07 17:04:09 -0700

    Drawing a Player Icon

--- main.py
+++ main.py
@@ -3,8 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
-
-
+from player import Player
 def main():
 
     pygame.init()
@@ -13,12 +12,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    dt = 0
+
     while 0 != 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
